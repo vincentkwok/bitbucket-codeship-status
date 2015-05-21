f136c5f62bef302b70fa05b2107cc65e5efb843a
Vincent Kwok <vincent.kwok@bindo.com>
2015-05-21 15:03:04 +0800

    widget url based on build id

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ module.exports = function () {
 	var bodyParser = require('body-parser');
 	var Request = require('request');
 	var basicAuth = require('basic-auth-connect');
+	var q = require('q');
 	var app = express();
 	
 	app.use('/media', express.static(__dirname + '/media'));
@@ -15,6 +16,50 @@ module.exports = function () {
 			consumer_key: process.env.OAUTH_KEY,
 			consumer_secret: process.env.OAUTH_SECRET
 		};
+	}
+
+	function getBuildCommitSha (pullRequest) {
+		if ( !((pullRequest.source) && (pullRequest.source.commit) && (pullRequest.source.commit.hash)) ) {
+			return null;
+		}
+		return pullRequest.source.commit.hash;
+	}
+
+	function getCodeshipBuildId (responseBody, buildCommitSha) {
+		var codeshipBuildId = null;
+
+		responseBody.builds.forEach(function (build, index) {
+			if (buildCommitSha.indexOf(build.commit_id) === 0) {
+				codeshipBuildId = build.id;
+			}
+		});
+		return codeshipBuildId;
+	}
+
+	function getWidgetUrlPromise (pullRequest, codeshipProjectId) {
+		var deferred = q.defer();
+		var buildCommitSha = getBuildCommitSha(pullRequest);
+		var baseWidgetUrl = 'https://codeship.io/projects/' + codeshipProjectId;
+
+		if (!buildCommitSha) {
+			deferred.resolve(baseWidgetUrl);
+		} else {
+			var get_codeship_project_url = 'https://codeship.com/api/v1/projects/' + codeshipProjectId + '.json?api_key=' + process.env.CODESHIP_API_KEY;
+			Request({
+				url: get_codeship_project_url,
+				method: 'GET'
+			}, function (err, response, body) {
+				if (err) {
+					deferred.reject(new Error(err));
+				} else {
+					var body = JSON.parse(response.body);
+					var codeshipBuildId = getCodeshipBuildId(body, buildCommitSha);
+
+					deferred.resolve(baseWidgetUrl + '/builds/' + codeshipBuildId);
+				}
+			});
+		}
+		return deferred.promise;
 	}
 
 	app.get('/', function (req, res) {
@@ -53,31 +98,34 @@ module.exports = function () {
 			res.status(400).end();
 			return;
 		}
-		
+
 		// if it doesn't already have Codeship status at the start of the description, let's add it
 		if (pullRequest.description.indexOf('[ ![Codeship Status') !== 0) {
-			var widget = '[ ![Codeship Status for ' + pullRequest.source.repository.full_name + '](https://codeship.io/projects/' + req.param('codeshipProjectUuid') +'/status?branch=' + pullRequest.source.branch.name + ')](https://codeship.io/projects/' + req.param('codeshipProjectId') + ')';
-			pullRequest.description = widget + '\r\n\r\n' + pullRequest.description;
-			
-			Request({
-				url: 'https://api.bitbucket.org/2.0/repositories/' + pullRequest.source.repository.full_name + '/pullrequests/' + pullRequest.id,
-				method: 'PUT',
-				oauth: oauth(),
-				json: pullRequest
-			}, function (err, response, body) {
-				if (err) {
-					res.status(500).end();
-					return;
-				}
+			var widgetUrlPromise = getWidgetUrlPromise(pullRequest, req.param('codeshipProjectId'));
+			widgetUrlPromise.then(function (widgetUrl) {
+				var widget = '[ ![Codeship Status for ' + pullRequest.source.repository.full_name + '](https://codeship.io/projects/' + req.param('codeshipProjectUuid') +'/status?branch=' + pullRequest.source.branch.name + ')](' + widgetUrl + ')';
+
+				pullRequest.description = widget + '\r\n\r\n' + pullRequest.description;
 				
-				if (response.body && response.body.error) {
-					console.log('response.body', response.body);
-					console.log('response.body.error', response.body.error);
-					res.status(500).end();
-					return;
-				}
-				
-				res.status(204).end();
+				Request({
+					url: 'https://api.bitbucket.org/2.0/repositories/' + pullRequest.source.repository.full_name + '/pullrequests/' + pullRequest.id,
+					method: 'PUT',
+					oauth: oauth(),
+					json: pullRequest
+				}, function (err, response, body) {
+					if (err) {
+						res.status(500).end();
+						return;
+					}
+					
+					if (response.body && response.body.error) {
+						console.log('response.body', response.body);
+						console.log('response.body.error', response.body.error);
+						res.status(500).end();
+						return;
+					}
+					res.status(204).end();
+				});
 			});
 		}
 		else {
